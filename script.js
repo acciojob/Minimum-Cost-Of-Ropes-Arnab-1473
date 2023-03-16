a89dc1f932870c14b47cb4446d6627fc94c412a6
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-03-16 21:53:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 function calculateMinCost() {
   //your code here
-  // Get the input rope lengths as a comma-separated string
-  const input = document.getElementById("rope-lengths").value;
-
-  // Convert the input string to an array of integers
-  const ropeLengths = input.split(",").map(Number);
-
-  // Initialize a priority queue with the input rope lengths
-  const pq = new MinPriorityQueue({ priority: (ropeLength) => ropeLength });
-  ropeLengths.forEach((length) => pq.enqueue(length));
-
-  // Keep track of the total cost and repeatedly combine the shortest ropes until only one rope remains
-  let totalCost = 0;
-  while (pq.size() > 1) {
-    // Dequeue the two shortest ropes
-    const shortestRope1 = pq.dequeue().element;
-    const shortestRope2 = pq.dequeue().element;
-
-    // Combine the ropes and enqueue the result
-    const combinedRopeLength = shortestRope1 + shortestRope2;
-    pq.enqueue(combinedRopeLength);
-
-    // Update the total cost
-    totalCost += combinedRopeLength;
+ const input = document.querySelector('#input').value;
+  const arr = input.split(',').map(num => parseInt(num));
+  
+  // create a min heap
+  const heap = new MinHeap();
+  for (let i = 0; i < arr.length; i++) {
+    heap.insert(arr[i]);
   }
 
-  // Display the total cost in the result div
-  const resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = `Minimum cost of connecting the ropes: ${totalCost}`;
-  
-  
+  let totalCost = 0;
+  while (heap.size() > 1) {
+    // extract the two smallest ropes
+    const rope1 = heap.extractMin();
+    const rope2 = heap.extractMin();
+
+    // join the ropes and add to total cost
+    const newRope = rope1 + rope2;
+    totalCost += newRope;
+
+    // insert the new rope back into the heap
+    heap.insert(newRope);
+  }
+
+  // display the result
+  const resultDiv = document.querySelector('#result');
+  resultDiv.textContent = totalCost;
 }  
